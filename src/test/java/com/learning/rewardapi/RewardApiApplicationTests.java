a26adfefbba74a7f9b2aec2f2f683a90f05459e2
Flavio Furlan <furlan@abap101.com>
2022-06-30 15:14:40 -0500

    initial commit - spring boot initializr

---
+++ src/test/java/com/learning/rewardapi/RewardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.rewardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
